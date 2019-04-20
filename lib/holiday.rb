b8cf19c100c11ec676f247bf80dc1e9729b8efc2
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-20 21:10:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -35,7 +35,10 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
- 
+ holiday_hash[:spring].each do |holiday, items|
+   items << supply
+   puts items
+ end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)

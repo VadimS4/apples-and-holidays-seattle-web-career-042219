a2090126e18579d5851a5e17adbb89a39a595f59
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-20 21:23:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,11 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect do |season, holidays|
     holiday.collect do |holidays, items|
       if items.include?("BBQ")
-        holiday_hash
+        holiday
+      end
+    end
+  end
+end
         
 
 

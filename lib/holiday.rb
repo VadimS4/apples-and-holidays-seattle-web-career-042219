b7170e27b5740dda8ebee4411a91fad390e44d4f
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-20 21:22:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,8 +71,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   
-  holiday_hash.collect do |holiday, items|
-    holiday.collect do |
+  holiday_hash.collect do |season, holidays|
+    holiday.collect do |holidays, items|
 
 
 

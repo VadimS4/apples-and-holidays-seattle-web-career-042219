5691ab01a97c0df9efb7f2abc1ff56b3ad163c41
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-20 21:35:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,9 +71,8 @@ def all_supplies_in_holidays(holiday_hash)
       supplies = supplies.join("")
       holiday = holiday.to_s.split("_").collect {|w| w.capitalize}.join(" ") << ":"
       puts " " + holiday + " " + supplies
-      
- 
-
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)

5812d172152d38c2cef0f1ca1512d3e78b16f131
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-20 21:30:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,8 +65,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
-  holiday_hash.collect do |season, holidays|
-    puts "#{season}: #{holidays}"
+  holiday_hash.each do |season, holiday|
+    
 
 
 end

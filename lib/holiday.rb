06892a55f652acbe6e109b12fcc90a3a2e7b7ded
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-20 21:36:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts season.to_s.capitalize! << ":"
     holiday.each do |holiday, supplies|
-      supplies = supplies.join("")
+      supplies = supplies.join(", ")
       holiday = holiday.to_s.split("_").collect {|w| w.capitalize}.join(" ") << ":"
       puts " " + holiday + " " + supplies
     end
